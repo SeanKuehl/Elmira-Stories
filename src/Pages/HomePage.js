da0af42579ade3e2ee66b342a2facf43d804fc2e
SeanKuehl <teddscottfelt@gmail.com>
2024-10-07 15:44:55 -0400

    added tree registration and image retrival routes

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,17 +1,46 @@
 
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+
+
+
 
 
 function HomePage() {
-  const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
+    const [treeId, setTreeID] = useState("");
+    const [dedicatedTo, setDedicatedTo] = useState("");
+    const [dedicatedBy, setDedicatedBy] = useState("");
+    const [dateAdded, setDateAdded] = useState("");
+    const [approximateLongitude, setApproximateLongitude] = useState("");
+    const [approximateLattitude, setApproximateLattitude] = useState("");
+    const [image, setImage] = useState("");
+
+
+    const [that, setThat] = useState("");
+
+    const fetchImage = async () => {
+        const res = await fetch('http://localhost:5000/get_tree_image/profile pic.jpg');
+        var body = await res.text();
+        
+        setThat(body);
+      };
+
+
+      useEffect(() => {
+        fetchImage();
+      }, []);
+
+
+    
+
+
     const handleOnSubmit = async (e) => {
         e.preventDefault();
         let result = await fetch(
-        'http://localhost:5000/register', {
+        'http://localhost:5000/register_new_memorial_tree', {
             method: "post",
-            body: JSON.stringify({ name, email }),
+            body: JSON.stringify({ treeId, dedicatedTo, dedicatedBy, approximateLattitude, approximateLongitude,  image }),
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -20,24 +49,52 @@ function HomePage() {
         console.warn(result);
         if (result) {
             alert("Data saved succesfully");
-            setEmail("");
-            setName("");
+            setTreeID("");
+            setDedicatedTo("");
+            setDedicatedBy("");
+            
+            setApproximateLongitude("");
+            setApproximateLattitude("");
+            setImage("");
         }
     }
     return (
         <>
             <h1>This is React WebApp </h1>
             <form action="">
-                <input type="text" placeholder="name" 
-                value={name} onChange={(e) => setName(e.target.value)} />
-                <input type="email" placeholder="email" 
-                value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="text" placeholder="tree ID" 
+                value={treeId} onChange={(e) => setTreeID(e.target.value)} />
+
+                <input type="text" placeholder="Dedicated To" 
+                value={dedicatedTo} onChange={(e) => setDedicatedTo(e.target.value)} />
+
+                <input type="text" placeholder="Dedicated By" 
+                value={dedicatedBy} onChange={(e) => setDedicatedBy(e.target.value)} />
+
+                <input type="text" placeholder="Approximate Longitude" 
+                value={approximateLongitude} onChange={(e) => setApproximateLongitude(e.target.value)} />
+
+                <input type="text" placeholder="Approximate Lattitude" 
+                value={approximateLattitude} onChange={(e) => setApproximateLattitude(e.target.value)} />
+
+                <input type="text" placeholder="Tree Image" 
+                value={image} onChange={(e) => setImage(e.target.value)} />
+
+
                 <button type="submit" 
                 onClick={handleOnSubmit}>submit</button>
             </form>
 
+
+            <img src={that} />
+
+            
+
+
         </>
     );
 }
+
+//https://elmirastoriestestbucket.s3.us-east-2.amazonaws.com/profile+pic.jpg
 
 export default HomePage;
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,6 +1,20 @@
 // Code  for mongoose config in backend
 // Filename - backend/index.js
 
+
+//const { GetObjectCommand, S3Client } = require('@aws-sdk/client-s3');
+var aws = require('aws-sdk');
+
+
+aws.config.setPromisesDependency(); //use so you can promisify to get the actual images
+            aws.config.update({
+                accessKeyId: "",
+                secretAccessKey: "",
+                region: ''
+            });
+
+
+
 // To connect with your mongoDB database
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/', {
@@ -14,25 +28,43 @@ mongoose.connect('mongodb://localhost:27017/', {
   .catch((error) => {
     console.log(error);
   });
-
+ 
 // Schema for users of app
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
+const MemorialTreeSchema = new mongoose.Schema({
+    tree_ID: {
         type: String,
         required: true,
         unique: true,
     },
-    date: {
+    dedicated_to: {
+        type: String,
+        required: true,
+    },
+    dedicated_by: {
+        type: String,
+        required: true,
+        default: "Elmira Lions Club"
+    },
+    date_added: {
         type: Date,
         default: Date.now,
     },
+    approximate_lattitude: {
+        type: String,   //float is not supported by Number, so just make it string
+        required: true,
+    },
+    approximate_longitude: {
+        type: String,
+        required: true,
+    },
+    tree_image: {
+        type: String,   //for now, this will be a local image path
+        required: true,
+    }
+
 });
-const User = mongoose.model('users', UserSchema);
-User.createIndexes();
+const Tree = mongoose.model('memorial_trees', MemorialTreeSchema);
+Tree.createIndexes();
 
 // For backend and express
 const express = require('express');
@@ -51,21 +83,47 @@ app.get("/", (req, resp) => {
     // backend working properly
 });
 
-app.post("/register", async (req, resp) => {
+app.post("/register_new_memorial_tree", async (req, resp) => {
     try {
-        const user = new User(req.body);
-        let result = await user.save();
+        //approximateLongitude, approximateLattitude, image
+        console.log(req.body);
+        const new_tree = new Tree({
+            tree_ID: req.body.treeId,
+            dedicated_to: req.body.dedicatedTo,
+            dedicated_by: req.body.dedicatedBy,
+            //date added has a good default, so no real need to pass it here
+            approximate_lattitude: req.body.approximateLongitude,
+            approximate_longitude: req.body.approximateLattitude,
+            tree_image: req.body.image,
+        });
+        let result = await new_tree.save();
         result = result.toObject();
         if (result) {
-            delete result.password;
             resp.send(req.body);
             console.log(result);
         } else {
-            console.log("User already register");
+            console.log("Tree already registered");
         }
 
     } catch (e) {
         resp.send("Something Went Wrong");
     }
 });
+
+
+app.get('/get_tree_image/:imageName', async (req, res) => {
+
+    const s3 = new aws.S3();
+
+    var params = {Bucket: '', Key: req.params.imageName};
+    var promise = s3.getSignedUrlPromise('getObject', params);
+    promise.then(function(url) {
+        res.send(url)
+    }, function(err) { console.log(err) });
+});
+
+
+
+
+
 app.listen(5000);
